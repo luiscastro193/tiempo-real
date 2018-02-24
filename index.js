2482a977f6df4bc772c83d32aa954875fa075756
Luis Castro Martín <luiscastro193@gmail.com>
2018-02-24 01:11:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+	socket.on('message', function(msg) {
+		io.emit('message', msg);
+	});
+});
+
+http.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
